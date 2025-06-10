e12357a42b1122dc41ab77c832bf581e1854e332
dhruvgoel3 <dhruvgoel1100@gmail.com>
2025-06-10 11:39:22 +0530

    ques

--- Two_pointer_problems/ques_1.java
+++ Two_pointer_problems/ques_1.java
@@ -12,26 +12,56 @@ public class ques_1 {
         System.out.println();
 
     }
+    static void swap(int arr[] , int i , int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
 
-    static void sortZerosAndOnes(int arr[]) {
+    }
+ 
+    static void sortZerosAndOnes(int arr[])
+    {
         int n = arr.length;
-        int zeros = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) {
-                zeros++;
-
+        int left = 0 ,  right = n-1;
+        while(left<right)
+        {
+            if(arr[left] == 1 && arr[right] == 0)
+            {
+                swap(arr,left,right);
+                left++;
+                right--;
             }
-        }
-        for (int i = 0; i < n; i++) {
-            if (i < zeros) {
-                arr[i] = 0;
-            } else {
-                arr[i] = 1;
+            if(arr[left] == 0)
+            {
+                left++;
             }
-
+            if(arr[right]== 1)
+            {
+                right--;
+            }
         }
 
     }
+    // static void sortZerosAndOnes(int arr[]) {
+    //     int n = arr.length;
+    //     int zeros = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         if (arr[i] == 0) {
+    //             zeros++;
+
+    //         }
+    //     }
+    //     for (int i = 0; i < n; i++) {
+    //         if (i < zeros) {
+    //             arr[i] = 0;
+    //         } else {
+    //             arr[i] = 1;
+    //         }
+
+    //     }
+
+    // }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
