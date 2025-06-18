5a7bb32db88b8c13b471e9cec82e4d36ff17a80f
dhruvgoel3 <dhruvgoel1100@gmail.com>
2025-06-18 16:15:25 +0530

    ques

--- Prefix_Sum/ques_1.java
+++ Prefix_Sum/ques_1.java
@@ -1,6 +1,31 @@
 package Prefix_Sum;
 //  Pattern :- Prefix sum questions / running sum 
 
+import java.util.Scanner;
+
 public class ques_1 {
-    
+    static int[] makePrefixSumArray(int arr[]) {
+        int n = arr.length;
+        int pref[] = new int[n];
+        pref[0] = arr[0];
+        for (int i = 0; i < n; i++) {
+            pref[i] = pref[i - 1] + arr[i];
+        }
+        return pref;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array:-");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter " + size + " elements of the array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(makePrefixSumArray(arr));
+
+    }
+
 }
